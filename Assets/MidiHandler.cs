4380f0e64f0ca0be25aa5d01caef53babaf1df55
Dan Sakamoto <contact@dansakamoto.com>
2017-06-19 19:14:07 -0400

    Added Midi control of blendshapes

---
+++ Assets/MidiHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class MidiHandler : MonoBehaviour {
+
+	public float[] knobs;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		for (int i = 0; i < knobs.Length; i++) {
+			knobs[i] = MidiMaster.GetKnob (i, 0f);
+		}
+
+	}
+}
---
+++ Assets/MidiToBlend.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MidiToBlend : MonoBehaviour {
+
+	public int[] patches;
+
+	SkinnedMeshRenderer smr;
+	Mesh skinnedMesh;
+
+	MidiHandler midiVal;
+
+	void Awake () {
+		smr = GetComponent<SkinnedMeshRenderer> ();
+		skinnedMesh = GetComponent<SkinnedMeshRenderer> ().sharedMesh;
+		midiVal = GameObject.Find ("MIDI Handler").GetComponent<MidiHandler> ();
+	}
+		
+	void Update () {
+
+		for (int i = 0; i < patches.Length; i++) {
+			smr.SetBlendShapeWeight (patches[i], midiVal.knobs[i]*100f);
+		}
+
+	}
+}
