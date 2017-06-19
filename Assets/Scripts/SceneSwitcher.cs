a77d3e6071b8414405877ed0622add18a729521c
Dan Sakamoto <contact@dansakamoto.com>
2017-06-19 17:49:14 -0400

    Added cueing system

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+
+		// CYCLING WITH SPACEBAR
+		if (Input.GetKeyDown (KeyCode.Space)) { //check if the user is pressing space 
+			// check what scene we’re currently in 
+			int sceneIndex = SceneManager.GetActiveScene ().buildIndex; 
+			// check if that matches the index of the last scene
+			if (sceneIndex == SceneManager.sceneCountInBuildSettings - 1) {
+				SceneManager.LoadScene (0); // if so, load the first scene
+			} else { 
+				SceneManager.LoadScene (sceneIndex + 1); // otherwise, load the next scene 
+			}
+		}
+		
+
+		// USING NUMBER KEYS
+		if( Input.GetKeyDown( KeyCode.Alpha1 ) ) {
+			SceneManager.LoadScene(0);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha2 ) ) {
+			SceneManager.LoadScene(1);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha3 ) ) {
+			SceneManager.LoadScene(2);
+		}
+
+		
+	}
+}
---
+++ Assets/Scripts/Unique.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unique : MonoBehaviour {
+
+	void Awake(){
+
+		// Object searches for an object with the same name as it + prefix “Unique”
+		GameObject testUnique = GameObject.Find("Unique "+this.gameObject.name);
+		if(testUnique==null){
+			// if it can’t find one, it appends “Unique” to it’s own name and becomes persistent
+			testUnique = this.gameObject;
+			testUnique.name = "Unique "+testUnique.name;
+			DontDestroyOnLoad(testUnique);
+		} else {
+
+			Destroy(this.gameObject); //another copy exists, object self-destructs
+		}
+	}
+
+}
