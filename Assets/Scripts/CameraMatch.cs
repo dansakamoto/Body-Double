e373ec5051cd3bb42d8686ef92fa86922bc63eb1
Dan Sakamoto <contact@dansakamoto.com>
2017-06-20 05:16:54 -0400

    First pass cam position from estimote values

--- Assets/Scripts/CameraMatch.cs
+++ Assets/Scripts/CameraMatch.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class CameraMatch : MonoBehaviour {
 
-	public GameObject cam;
+	public GameObject cam, camHousing;
+	Quaternion offset;
+
+	public void setOffset(){
+		offset = transform.rotation;
+		camHousing.transform.rotation = Quaternion.Inverse (offset);// * Quaternion.Euler(90.0f, 0.0f, 0.0f);
+	}
 
 	// Use this for initialization
 	void Start () {
 		cam = GameObject.Find ("Wide Camera");
+		camHousing = GameObject.Find ("Camera Housing");
+		//offset = transform.rotation;
+		offset = Quaternion.Euler (0f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Quaternion q = transform.rotation;
-		cam.transform.localRotation = new Quaternion(q.x, q.y, q.z, q.w);
+		//Quaternion q = transform.rotation;
+		cam.transform.localRotation = transform.rotation; // * Quaternion.Inverse (offset) * Quaternion.Euler(-90.0f, 0.0f, 0.0f);
+		//camHousing.transform.localRotation = transform.rotation * Quaternion.Inverse(offset) * Quaternion.Euler(90.0f, 0.0f, 0.0f);
 	}
 }
---
+++ Assets/Scripts/EstimotePass.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class EstimotePass : NetworkBehaviour {
+
+	public double us = 0, ds = 0, sl = 0, sr = 0;
+	double oldus = 0, oldds = 0, oldsl = 0, oldsr = 0;
+	float currentx = 0f, currentz = 0f;
+	float targetx = 0f, targetz = 0f;
+
+	public float smoothing = 10f, snapping = .001f, sensitivity = .1f, slmod = 1f, srmod=2.1f, usmod=3f, dsmod=1.2f;
+
+	//public float xSensitivity = 1f, zSensitivity = 1f;
+	GameObject camHousing, cam;
+
+	protected void Start(){
+		camHousing = GameObject.Find ("Camera Housing");
+		cam = GameObject.Find ("Wide Camera");
+	}
+
+	protected void Update(){
+
+		if (oldsl != sl || oldsr != sr) {
+			targetx = (float)((sr*srmod) - (sl*slmod)) / 2f;
+		}
+
+		if (oldus != us || oldds != ds) {
+			targetz = (float)((ds*dsmod) - (us*usmod)) / 2f;
+		}
+
+		if (currentx != targetx) {
+
+			if (Mathf.Abs (targetx - currentx) < snapping) {
+				currentx = targetx;
+			} else {
+				currentx += ((targetx - currentx) / smoothing);
+			}
+		}
+
+		if (currentz != targetz) {
+			if (Mathf.Abs (targetz - currentz) < snapping){
+				currentz = targetz;
+			} else {
+				currentz += ((targetz - currentz) / smoothing);
+			}
+		}
+
+
+		camHousing.transform.position = new Vector3 (currentx*sensitivity, camHousing.transform.position.y, currentz*sensitivity);
+	}
+
+	[Command]
+	public void CmdSetUS(double x){
+		us = x;
+	}
+
+	[Command]
+	public void CmdSetDS(double x){
+		ds = x;
+	}
+
+	[Command]
+	public void CmdSetSR(double x){
+		sr = x;
+	}
+
+	[Command]
+	public void CmdSetSL(double x){
+		sl = x;
+	}
+
+	[Command]
+	public void CmdSetOffset(){
+		//setOffset = true;
+		//Debug.Log("It worked");
+		//camHousing.transform.Rotate(new Vector3(0f, 0f, cam.transform.rotation.eulerAngles.z*-1f));
+		this.GetComponent<CameraMatch>().setOffset();
+	}
+
+}
