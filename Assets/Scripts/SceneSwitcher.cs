1fec638be00b8bc0674c246e08725b203efee00a
Body Double Workstation <contact@dansakamoto.com>
2017-06-20 22:54:34 -0400

    End of day list

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -38,6 +38,34 @@ public class SceneSwitcher : MonoBehaviour {
 			SceneManager.LoadScene(2);
 		}
 
+		if( Input.GetKeyDown( KeyCode.Alpha4 ) ) {
+			SceneManager.LoadScene(3);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha5 ) ) {
+			SceneManager.LoadScene(4);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha6 ) ) {
+			SceneManager.LoadScene(5);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha7 ) ) {
+			SceneManager.LoadScene(6);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha8 ) ) {
+			SceneManager.LoadScene(7);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha9 ) ) {
+			SceneManager.LoadScene(8);
+		}
+
+		if( Input.GetKeyDown( KeyCode.Alpha0 ) ) {
+			SceneManager.LoadScene(9);
+		}
+
 		
 	}
 }
