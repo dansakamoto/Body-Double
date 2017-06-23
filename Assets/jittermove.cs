194bd778aa9b9c98a0143980d4f10c0b81564b27
Dan Sakamoto <contact@dansakamoto.com>
2017-06-23 06:17:52 -0400

    Desire Monster Scenes

---
+++ Assets/jittermove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jittermove : MonoBehaviour {
+
+	float startx, starty, startz;
+	GameObject anchorCube;
+	Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+		startx = transform.rotation.eulerAngles.x;
+		starty = transform.rotation.eulerAngles.y;
+		startz = transform.rotation.eulerAngles.z;
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler (startx + (Random.value - .5f), starty, startz);
+	}
+}
