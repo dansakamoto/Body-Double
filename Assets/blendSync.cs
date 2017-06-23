b8fcf2e4ee689deb7f185d4cd3e01464ab7a1972
Dan Sakamoto <contact@dansakamoto.com>
2017-06-23 11:09:23 -0400

    Added blendSync

---
+++ Assets/blendSync.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blendSync : MonoBehaviour {
+
+	SkinnedMeshRenderer syncRef, mySkin;
+
+	// Use this for initialization
+	void Start () {
+		syncRef = GameObject.FindGameObjectWithTag ("BlendRef").GetComponent<SkinnedMeshRenderer>();
+		mySkin = GetComponent<SkinnedMeshRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < 136; i++) {
+			mySkin.SetBlendShapeWeight (i, syncRef.GetBlendShapeWeight (i));
+		}
+	}
+}
+
+
