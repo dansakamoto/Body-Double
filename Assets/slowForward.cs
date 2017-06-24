2db143b18ee750df583825c1e66fc4be7029437b
Dan Sakamoto <contact@dansakamoto.com>
2017-06-24 00:44:30 -0400

    More cloth tests

---
+++ Assets/slowForward.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slowForward : MonoBehaviour {
+
+	public bool forward = true;
+	public int speed = 1;
+	int rate;
+
+	// Use this for initialization
+	void Start () {
+		rate = 2000 / speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (forward) {
+			transform.position = transform.position + (Vector3.forward / rate);
+		} else {
+			transform.position = transform.position + (Vector3.back / rate);
+		}
+	}
+}
